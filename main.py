d5a310caef3d0c69a539f6942c7a71e72816a58f
thecreature <121248374+AriadnaGonzale5@users.noreply.github.com>
2023-02-18 23:56:50 +0000

    hi

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import math
 class Vector2d:
+
     def __init__(self,x,y):
         self.x =x
         self.y =y
 
     def __repr__(self):
         return f"Vector2d(x ='{self.x}', y={self.y})"
+    def __str__(self):
+        return f"('{self.x}j + {self.y}j')"
+    def __abs__(self):
+        tempx =self.x**2
+        tempy = self.y**2
+        soon = tempx + tempy
+        done = math.sqrt(soon)
+        return done
